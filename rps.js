dc02d758c9074ef1d9f482e0ac51264751c4135a
Lucas LaVilla <lucas.lavilla@mail.com>
2024-06-07 19:14:48 -0400

    Completed RPS game with ui

--- rps.js
+++ rps.js
@@ -22,10 +22,84 @@ WRITE LOGIC TO PLAY ENTIRE GAME
 2. move 'playRound' function and score varaibles inside of 'playGame' function
 3. play 5 rounds by declaring 'playRound' 5 times
 */
+document.addEventListener("DOMContentLoaded", function(){
+
+const title = document.createElement('h1');
+title.textContent = "Rock Paper Scissors Game";
+title.style.textAlign = "center";
+document.body.appendChild(title)
+
+    const btnContainer = document.createElement('div');
+    btnContainer.style.display = "flex";
+    btnContainer.style.alignItems = "center";
+    btnContainer.style.justifyContent = "center";
+    btnContainer.style.gap = "10px";
+    
+    
+    
+    const rockButton = document.createElement('button');
+    rockButton.style.width = "100px";
+    rockButton.style.color = "white";
+    rockButton.style.backgroundColor = "grey";
+    rockButton.textContent = "Rock";
+    rockButton.addEventListener('click', function(){
+        playRound("rock", getComputerChoice());
+    });
+
+    const paperButton = document.createElement('button');
+    paperButton.style.width = "100px";
+    paperButton.style.backgroundColor = "light gray";
+    paperButton.textContent = "Paper";
+    paperButton.addEventListener('click', function(){
+        playRound("paper", getComputerChoice());
+    });
+
+    const scissorsButton = document.createElement('button');
+    scissorsButton.style.width = "100px";
+    scissorsButton.style.color = "white";
+    scissorsButton.style.backgroundColor = "red";
+    scissorsButton.textContent = "Scissors";
+    scissorsButton.addEventListener('click', function(){
+        playRound("scissors", getComputerChoice())
+    });
+
+    btnContainer.appendChild(rockButton);
+    btnContainer.appendChild(paperButton);
+    btnContainer.appendChild(scissorsButton);
+
+    document.body.appendChild(btnContainer);
+
+    const resultsDiv = document.createElement('div');
+    resultsDiv.style.textAlign = "center";
+    resultsDiv.style.marginTop = "30px";
+    resultsDiv.setAttribute('id', 'results');
+    document.body.appendChild(resultsDiv);
+
+
+});
 
 let humanScore = 0;
 let computerScore = 0;
 
+function updateResults(result) {
+    const resultsDiv = document.getElementById('results');
+    resultsDiv.textContent = ` | You: ${humanScore} | Computer: ${computerScore} | \n${result}\n`;
+
+    if(humanScore === 5) {
+        resultsDiv.textContent += "\nCongratulation! You win!\n";
+        resetGame();
+    }
+    else if (computerScore === 5) {
+        resultsDiv.textContent += "\nSorry, the computer wins!\n";
+        resetGame();
+    }
+
+}
+
+function resetGame() {
+    humanScore = 0;
+    computerScore = 0;
+}
 
 function getComputerChoice(){
     let comChoice = (Math.floor(Math.random() * 3));
@@ -48,33 +122,34 @@ function getHumanChoice(){
 
 function playRound(humanChoice, computerChoice){
     if(computerChoice === "rock" && humanChoice === "scissors"){
-        console.log("Rock beats scissors. The computer has won.") 
+        updateResults("Rock beats scissors. The computer has won this round.") 
         computerScore += 1;
     }
     else if(computerChoice === "scissors" && humanChoice === "rock"){
-        console.log("Rock beats scissors. You won!!!")
+        updateResults("Rock beats scissors. You won this round!")
         humanScore += 1;
     }
     else if(computerChoice === "paper" && humanChoice === "rock"){
-        console.log("Paper beats rock. The computer has won.")
+        updateResults("Paper beats rock. The computer has won this round.")
         computerScore += 1;
     }
     else if(computerChoice === "rock" && humanChoice === "paper"){
-        console.log("Paper beats rock. You won!!!")
+        updateResults("Paper beats rock. You won this round!")
         humanScore += 1;
     }
     else if(computerChoice === "scissors" && humanChoice === "paper"){
-        console.log("Scissors beats paper. The computer has won.")
+        updateResults("Scissors beats paper. The computer has won this round.")
         computerScore += 1;
     }
     else if(computerChoice === "paper" && humanChoice === "scissors"){
-        console.log("Scissors beats paper. You won!!!")
+        updateResults("Scissors beats paper. You won this round!")
         humanScore += 1;
     }
     else{
-        console.log("It was a draw...")
+        updateResults("This round was a draw...")
     }
 }
+/* //  Removing playGame() function for now!!!
 function playGame(){
     for(let x = 1; x <= 5; x++){
         const humanSelection = getHumanChoice();
@@ -95,3 +170,4 @@ function playGame(){
 }
 
 playGame();
+*/
